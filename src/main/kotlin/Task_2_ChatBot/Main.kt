7ddc8d0c383e4bc497e7f27882d9d80f15f52269
Vladyslav Nesterov <nesterow@i.ua>
2023-03-25 18:47:22 +0100

    Task 1 - ready Task 2 - ready Task 3 - ready Task 4 - ready Task 5 - ready Task 6 - in progress...

--- src/main/kotlin/Task_2_ChatBot/Main.kt
+++ src/main/kotlin/Task_2_ChatBot/Main.kt
@@ -3,59 +3,65 @@ package Task_2_ChatBot
 import java.lang.System.`in`
 import java.util.Scanner
 
-        fun task1() {
-            val sc = Scanner(`in`)
+fun task1() {
+    val sc = Scanner(`in`)
 
-            print("please, write your number: ")
-            val num = sc.nextInt()
+    print("please, write your number: ")
+    val num = sc.nextInt()
 
-            if (num % 2 == 0)
-                println("$num is even")
-            else
-                println("$num is odd")
-        }
+    if (num % 2 == 0)
+        println("$num is even")
+    else
+        println("$num is odd")
+}
 
-        fun task2() {
-            val sc = Scanner(`in`)
+fun task2() {
+    val sc = Scanner(`in`)
 
-            print("please, write your number: ")
-            val num = sc.nextInt()
+    print("please, write your number: ")
+    val num = sc.nextInt()
 
-            if (num % 2 == 0)
-                println("Answer is: " + num * 4)
-            else
-                println("Answer is: " + num * 5)
-        }
+    if (num % 2 == 0)
+        println("Answer is: " + num * 4)
+    else
+        println("Answer is: " + num * 5)
+}
 
-        fun task3() {
-            val sc = Scanner(`in`)
-            val zero = 0
+fun task3() {
+    val sc = Scanner(`in`)
+    val zero = 0
 
-            print("please, write your number: ")
-            val num = sc.nextInt()
-            if (num > zero)
-                println("Answer is: " + (num.inv() + 1)) //.inv - логическое отрицание или инверсия - инвертирует биты числа
-            if (num < zero)
-                println("Answer is: " + num)
-            else
-                println("It's difficult question, but I think - it will be: " + zero)
-        }
+    print("please, write your number: ")
+    val num = sc.nextInt()
+    if (num > zero)
+        println("Answer is: " + (num.inv() + 1)) //.inv - логическое отрицание или инверсия - инвертирует биты числа
+    if (num < zero)
+        println("Answer is: " + num)
+    else
+        println("It's difficult question, but I think - it will be: " + zero)
+}
 
-        fun task4(){
-            val sc = Scanner(`in`)
+fun task4(){
+    fun century(year: Int): Int {
+        return if (year % 100 == 0) year / 100
+        else (year / 100) + 1
+    }
+    println("Your century is: " + century(1950)) //Нужно вводить вручную год, со сканнером не работает ;(
+}
 
-            println("Write year to check century: ")
-            val year = sc.nextInt()
+fun task5(){ //метод сортировки
+    val arr: Array<Int> = arrayOf(94, 37, 20, 33, 14, 43, 100,
+        61, 59, 46, 39, 28, 59,88, 35)
 
-            if(year <= 0)
-             println("0 and negative is not allow "
-                     + "for a year")
-            if(year <= 100)
-               println("1st century\n")
+    arr.sort()
 
+    println("Minimal array value is: ${arr.first()}")
+}
 
+fun task6(){
 
-        }
+}
+
 fun main(){
 
         }
